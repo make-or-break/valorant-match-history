9d3d5885b93da96b437aafe018e59718611e9da7
MayNiklas <info@niklas-steffen.de>
2022-07-09 15:10:15 +0200

    add elo stats methods

--- src/match_crawler/get_elo.py
+++ src/match_crawler/get_elo.py
@@ -1,4 +1,14 @@
+import time
+
 from match_crawler.database import sql_statements as db
+
+
+def get_match_last(puuid, n):
+    """
+    Get the n matches ago match of a player
+    """
+
+    return db.get_matches_by_puuid_last(puuid, n)[n - 1]
 
 
 def get_match_elo(puuid, n):
@@ -6,7 +16,15 @@ def get_match_elo(puuid, n):
     Get the ELO of a match n matches ago.
     """
 
-    return db.get_matches_by_puuid_last(puuid, n)[n - 1].match_elo
+    return get_match_last(puuid, n).match_elo
+
+
+def get_match_date(puuid, n):
+    """
+    Get the datch of a match n matches ago.
+    """
+
+    return get_match_last(puuid, n).match_start
 
 
 def get_elo_over_matches(puuid, n):
@@ -15,3 +33,37 @@ def get_elo_over_matches(puuid, n):
     """
 
     return get_match_elo(puuid, 1) - get_match_elo(puuid, n + 1)
+
+
+def matches_within_time(puuid, n):
+    """
+    Finde the number of matches within a timespan!
+    """
+
+    # days in seconds
+    seconds = n * 24 * 60 * 60
+
+    matches = 0
+
+    while time.time() - int(get_match_date(puuid, matches + 1)) < seconds:
+        matches += 1
+
+    return matches
+
+
+def get_elo_over_time(puuid, n):
+    """
+    Get the ELO diff over n days
+    """
+
+    return get_elo_over_matches(puuid, matches_within_time(puuid, n))
+
+
+if __name__ == "__main__":
+    """
+    for testing this module isolated
+    """
+
+    puuid = "d515e2d5-b50e-5c77-a79e-eeb46dbe488a"
+    # get elo diff last 2 days
+    print(get_elo_over_time(puuid, 1))
