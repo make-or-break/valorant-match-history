7aa0437ac4edf66a25ff8c962ae086c56ba90ea6
MayNiklas <info@niklas-steffen.de>
2022-07-09 13:57:03 +0200

    update __init__.py

--- src/match_crawler/__init__.py
+++ src/match_crawler/__init__.py
@@ -1,5 +1,7 @@
 from .database.sql_statements import add_user
+from .database.sql_statements import get_tracked_status
 from .database.sql_statements import get_tracked_users
+from .database.sql_statements import get_user_by_puuid
 from .database.sql_statements import update_tracking
 from .database.sql_statements import user_exists
 from .main import check_new_matches
